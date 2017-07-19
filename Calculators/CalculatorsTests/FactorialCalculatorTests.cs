99799f9fa9d0510a68686da12c794c5c754a654a
david noakes <david_noakes@dialog.com.au>
2017-07-19 14:45:09 +1000

    add unit tests

---
+++ Calculators/CalculatorsTests/FactorialCalculatorTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculators.Tests
+{
+    [TestClass()]
+    public class FactorialCalculatorTests
+    {
+        [TestMethod()]
+        public void CalculateFactorialTest()
+        {
+            FactorialCalculator Fred = new FactorialCalculator();
+            Assert.AreEqual(0, Fred.CalculateFactorial(0));
+            Assert.AreEqual(1, Fred.CalculateFactorial(1));
+            Assert.AreEqual(-1, Fred.CalculateFactorial(-10));
+            Assert.AreEqual(2, Fred.CalculateFactorial(2));
+            Assert.AreEqual(6, Fred.CalculateFactorial(3));
+            Assert.AreEqual(120, Fred.CalculateFactorial(5));
+            Assert.AreEqual(3628800, Fred.CalculateFactorial(10));
+            Assert.AreEqual(39916800, Fred.CalculateFactorial(11));
+            Assert.AreEqual(479001600, Fred.CalculateFactorial(12));
+            Assert.AreNotEqual(6227020800, Fred.CalculateFactorial(13));
+            Assert.AreNotEqual(87178291200, Fred.CalculateFactorial(14));
+            Assert.AreEqual(-2, Fred.CalculateFactorial(15));
+            Assert.AreEqual(-3, Fred.CalculateFactorial(256));
+
+        }
+    }
+}
