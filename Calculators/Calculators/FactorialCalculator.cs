9e381bb6f7cc37fd183354c5eb3c602d989ab105
david noakes <david_noakes@dialog.com.au>
2017-07-19 11:44:31 +1000

    initial coding for calculators

---
+++ Calculators/Calculators/FactorialCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculators
+{
+    public class FactorialCalculator
+    {
+        public int CalculateFactorial(int num)
+        {
+            int CalcResult = num;
+            if (num <= 0)
+            {
+                return 0;
+            }
+            if (num > 32768)
+            {
+                return -1;
+            }
+            for (int i=num-1; i>0;i--)
+            {
+                CalcResult = CalcResult * i;
+            }
+            return CalcResult;
+        }
+    }
+}
