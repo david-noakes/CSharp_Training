5a9a83a7c3bfccb62d35019b96018e8bfbb3e375
david noakes <david_noakes@dialog.com.au>
2017-07-31 17:50:46 +1000

    creating a custom helper in code using the new Helpers folder

---
+++ VS2015/WebApp_MVC/WebApp_MVC/Helpers/CustomHelper1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp_MVC.Helpers
+{
+    public static class CustomHelper1
+    {
+        public static MvcHtmlString customHTMLStripper(this HtmlHelper html, string htmlInput)
+        {
+            return new MvcHtmlString(System.Text.RegularExpressions.Regex.Replace(htmlInput, "<.*?>", string.Empty));
+        }
+    }
+}
