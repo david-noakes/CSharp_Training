5db7b248cc7a641c9f53775d78e6771b4ff80c29
david noakes <david_noakes@dialog.com.au>
2017-08-01 14:31:08 +1000

    using JQuery to modify HTML Elements

--- VS2015/WebApp_MVC/WebApp_MVC/Scripts/customJS.js
+++ VS2015/WebApp_MVC/WebApp_MVC/Scripts/customJS.js
@@ -1,9 +1,23 @@
-﻿$("#pane").position({
-    my: "centre",
-    at: "centre",
-    of: "paneContainer"
+﻿$('#pane').position({
+    my: 'center',
+    at: 'center',
+    of: 'paneContainer'
 });
 
 function togglePane() {
-    $("#pane").toggle("bounce", { distance: 100, times: 9 }, "slow");
+    $('#pane').toggle('bounce', { distance: 100, times: 9 }, 'slow');
+}
+
+function changeVal() {
+    var textVal = $('#field1').val();
+    $('#display').html(textVal);
+}
+
+function changeStyle() {
+    $('#display').css('background-color', '#01ff03'); // neon green
+}
+
+function clearAll() {
+    $('#display').html('Sample message appears here');
+    $('#display').css('background-color', '#efeeef'); // light grey
 }
