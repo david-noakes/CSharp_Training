761d5ab5af0b3b02f70da3a7c752f44623b9d4a2
david noakes <david_noakes@dialog.com.au>
2017-08-03 08:43:18 +1000

    rename WebApplMVC_3 to WebAppMVC_3, and add in WebAppMVC_4 for "planning an adaptive UI Layout"

---
+++ VS2015/WebAppMVC_4/WebAppMVC_4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppMVC_4.Startup))]
+namespace WebAppMVC_4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
