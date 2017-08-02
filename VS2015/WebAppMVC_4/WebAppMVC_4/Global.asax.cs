24e98f7d40fee31f43ed46f5ee59dc96ee60a2d2
david noakes <david_noakes@dialog.com.au>
2017-08-03 09:54:37 +1000

    Using custom Display modes for smart phones - use the phone name string as part of the title - register with Global.asax

--- VS2015/WebAppMVC_4/WebAppMVC_4/Global.asax.cs
+++ VS2015/WebAppMVC_4/WebAppMVC_4/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.WebPages;
 
 namespace WebAppMVC_4
 {
@@ -16,6 +17,16 @@ namespace WebAppMVC_4
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            DisplayModeProvider.Instance.Modes.Insert(0, new DefaultDisplayMode("iPhone")
+            {
+                ContextCondition = (ctx => ctx.GetOverriddenUserAgent().IndexOf("iPhone", StringComparison.OrdinalIgnoreCase) >= 0)
+            });
+
+            DisplayModeProvider.Instance.Modes.Insert(0, new DefaultDisplayMode("Nexus")
+            {
+                ContextCondition = (ctx => ctx.GetOverriddenUserAgent().IndexOf("Nexus", StringComparison.OrdinalIgnoreCase) >= 0)
+            });
         }
     }
 }
