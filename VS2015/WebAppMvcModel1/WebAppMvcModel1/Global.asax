207db8cddafefb84cec0be08a113d4f0e09954b0
david noakes <david_noakes@dialog.com.au>
2017-08-03 12:08:47 +1000

    Initial load of base project for "Working with ASP.NET models

---
+++ VS2015/WebAppMvcModel1/WebAppMvcModel1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppMvcModel1.MvcApplication" Language="C#" %>
---
+++ VS2015/WebAppMvcModel1/WebAppMvcModel1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppMvcModel1.Startup))]
+namespace WebAppMvcModel1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
