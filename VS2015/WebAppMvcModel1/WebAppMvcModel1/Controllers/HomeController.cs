3ff047d1a8b590cb2d0383f9dc3032ae896df64e
david noakes <david_noakes@dialog.com.au>
2017-08-03 14:11:59 +1000

    creating a new MVC model class - supply information to a page

--- VS2015/WebAppMvcModel1/WebAppMvcModel1/Controllers/HomeController.cs
+++ VS2015/WebAppMvcModel1/WebAppMvcModel1/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebAppMvcModel1.Models;
 
 namespace WebAppMvcModel1.Controllers
 {
@@ -25,6 +26,16 @@ namespace WebAppMvcModel1.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult GetPlayInfo()
+        {
+            ViewBag.Message = "Displaying model property values in a view";
+            ModelInformation someInfo = new ModelInformation();
+            someInfo.Title = "The Secret Garden";
+            someInfo.Synopsis = "A novel written by Francis Hodgeson";
+
+            return View("ModelInfo", someInfo);
         }
     }
 }
---
+++ VS2015/WebAppMvcModel1/WebAppMvcModel1/Models/ModelInformation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppMvcModel1.Models
+{
+    public class ModelInformation
+    {
+        public int PlayID { get; set; }
+        public string Title { get; set; }
+        public string Synopsis { get; set; }
+    }
+}
