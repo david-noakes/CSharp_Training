3474aedd7b60bb69b407359e75cbc206e1c30db4
david noakes <david_noakes@dialog.com.au>
2017-08-15 11:28:34 +1000

    Working with Models (entity framework) base app with model model generated from Northwind database after MVC app created

---
+++ VS2015/MVCModelBinding1/MVCModelBinding1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCModelBinding1.MvcApplication" Language="C#" %>
---
+++ VS2015/MVCModelBinding1/MVCModelBinding1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCModelBinding1.Startup))]
+namespace MVCModelBinding1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
