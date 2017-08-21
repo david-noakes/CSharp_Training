3c3ebfa072a7b013f97ce31aeb3eac4e132eaf5a
david noakes <david_noakes@dialog.com.au>
2017-08-21 14:34:48 +1000

    Working with ASP.NET Web API Controllers. Generated Code only

---
+++ VS2015/WebAppAPI2/WebAppAPI2/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebAppAPI2.Startup))]
+
+namespace WebAppAPI2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
