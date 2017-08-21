a7b52ab63bfe3c611bfd2b33d4a65d9304014dc5
david noakes <david_noakes@dialog.com.au>
2017-08-21 11:12:22 +1000

    Web API default generated code

---
+++ VS2015/WebAppAPI1/WebAppAPI1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppAPI1.WebApiApplication" Language="C#" %>
---
+++ VS2015/WebAppAPI1/WebAppAPI1/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebAppAPI1.Startup))]
+
+namespace WebAppAPI1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
