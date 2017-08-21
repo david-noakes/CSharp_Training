96159a26165fd9e41d850b88680e2427d98828a1
david noakes <david_noakes@dialog.com.au>
2017-08-21 15:22:19 +1000

    Implementing HTTP GET

---
+++ VS2015/WebAppAPI2/WebAppAPI2/Controllers/SupplierController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using WebAppAPI2.Models;
+
+namespace WebAppAPI2.Controllers
+{
+    public class SupplierController : ApiController
+    {
+        Supplier[] suppliers = new Supplier[5]
+        {
+            new Supplier {CompanyName="Mars Farm", City="New Seattle", Country="Mars", SupplierId = 1},
+            new Supplier {CompanyName="Palamedes' Produce", City="New New York", Country="Mars", SupplierId = 2},
+            new Supplier {CompanyName="Brocadero", City="Tijuana", Country="Mexico", SupplierId = 3},
+            new Supplier {CompanyName="Wakayama Ramen", City="Nova Moskva", Country="Luna", SupplierId = 4},
+            new Supplier {CompanyName="Steel Magnolia", City="Marsopolis", Country="Mars", SupplierId = 5}
+        };
+
+        // GET api/supplier
+        public IEnumerable<Supplier> GetAllSuppliers()
+        {
+            return suppliers;
+            //return View("Index", suppliers);  <-- not defined for apiController
+        }
+
+        // GET api/supplier/5
+        public Supplier GetSupplierById(int id)
+        {
+            var supplier = suppliers.FirstOrDefault((p) => p.SupplierId == id);
+            return supplier;
+        }
+    }
+}
---
+++ VS2015/WebAppAPI2/WebAppAPI2/Models/Supplier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppAPI2.Models
+{
+    public class Supplier
+    {
+        public int SupplierId { get; set; }
+        public string CompanyName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
