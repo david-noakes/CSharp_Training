5610592b2b9104778700cd03c3a05a52b43dcf5b
david noakes <david_noakes@dialog.com.au>
2017-08-23 11:34:44 +1000

    DB context and Initializer

---
+++ VS2015/WebAppJson1/WebAppJson1/Models/TheDBContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebAppJson1.Models
+{
+    public class TheDBContext : DbContext
+    {
+        public static string theConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=WebAppJson;Integrated Security=True";
+        public TheDBContext(string nameOrConnectionString) : base(nameOrConnectionString)
+        {
+        }
+        public TheDBContext() : base(theConnectionString)
+        {
+        }
+
+        public DbSet<Supplier> Supplier { get; set; }
+    }
+
+    [Table("Supplier")]
+    public class Supplier
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int SupplierId { get; set; }
+
+        [Display(Name = "Company Name")]
+        public string CompanyName { get; set; }
+        [Display(Name = "Supplier City")]
+        public string City { get; set; }
+        [Display(Name = "Supplier Country")]
+        public string Country { get; set; }
+
+
+    }
+}
---
+++ VS2015/WebAppJson1/WebAppJson1/Models/TheDBInitializer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebAppJson1.Models;
+
+namespace WebAppJson1.Models
+{
+    public class TheDBInitializer : DropCreateDatabaseAlways<TheDBContext>
+    {
+        private DbContext dbContext;
+
+        public TheDBInitializer(DbContext dbContext) : base()
+        {
+            this.dbContext = dbContext;
+        }
+        public TheDBInitializer() : base()
+        {
+            this.dbContext = new TheDBContext();
+        }
+
+
+        protected override void Seed(TheDBContext context)
+        {
+            base.Seed(context);
+            // Create supplier records
+            var suppliers = new List<Supplier>
+            {
+                new Supplier {CompanyName="Mars Farm", City="New Seattle", Country="Mars"},
+                new Supplier {CompanyName="Palamedes' Produce", City="New New York", Country="Mars"},
+                new Supplier {CompanyName="Brocadero", City="Tijuana, Mexico", Country="Earth"},
+                new Supplier {CompanyName="Wakayama Ramen", City="Nova Moskva", Country="Luna"},
+                new Supplier {CompanyName="Steel Magnolia", City="Marsopolis", Country="Mars"},
+                new Supplier {CompanyName="Mental As Anything", City="Lunopolis", Country="Luna"},
+                new Supplier {CompanyName="Fugue State", City="Erehwon", Country="Shadowlands"},
+                new Supplier {CompanyName="Edge of Realty", City="Weeping Waste", Country="Shadowlands"}
+
+            };
+            suppliers.ForEach(s => context.Supplier.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
