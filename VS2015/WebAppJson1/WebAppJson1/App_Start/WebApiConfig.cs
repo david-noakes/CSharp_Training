716b9957604b95034b2370bfb7dd16ca1fd35060
david noakes <david_noakes@dialog.com.au>
2017-08-23 12:37:38 +1000

    preserve object references for XML

--- VS2015/WebAppJson1/WebAppJson1/App_Start/WebApiConfig.cs
+++ VS2015/WebAppJson1/WebAppJson1/App_Start/WebApiConfig.cs
@@ -5,6 +5,8 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using System.Runtime.Serialization;
+using WebAppJson1.Models;
 
 namespace WebAppJson1
 {
@@ -39,14 +41,23 @@ namespace WebAppJson1
             // set XML formattwe to write indented XML
             xml.Indent = true;
 
+            // preserve object references when circular references are found
+            json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.All;
+
+            // preserve object refences for circular references in xml
+            var myDocs = new DataContractSerializer(
+                /* type */ typeof(Product),
+                /* known types */ null,
+                /* maxItemsIngraph */ int.MaxValue,
+                /* ignoreExtensionsData */ false,
+                /* preserveObjectRefs */ true,
+                /* dataContractSurrogate */ null
+                );
+            xml.SetSerializer<Product>(myDocs);
             // remove XML formatter
             mediaTypeFormatters.Remove(xml);
             // OR remove json for xml
             //mediaTypeFormatters.Remove(json);
-
-            // preserve object references when circular references are found
-            json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.All;
-
 
 
         }
